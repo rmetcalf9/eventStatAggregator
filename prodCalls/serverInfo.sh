98947762afa87137ff38237f0a1dd4ecb9092540
Robert Metcalf <rmetcalf9@googlemail.com>
2021-01-03 13:08:39 +0000

    Adding serverinfo example call

---
+++ prodCalls/serverInfo.sh
@@ -0,0 +1,10 @@
+curl -X GET \
+  https://api.metcarob.com/eventstataggregator/v0/public/api/info/serverinfo \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 9c21eeb7-a9d1-65cf-38cc-d8688882f28c' \
+  -d '{
+  "start": "2021-01-01T15:50-04:00",
+  "end": "2021-03-01T15:50-04:00"
+}'
+
